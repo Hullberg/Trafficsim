b35a1235313006b5b1255cb3a95ea349de1927b5
Hullberg <johan.hullberg@gmail.com>
2014-11-04 10:00:09 +0100

    Lane.java klar

--- Light.java
+++ Light.java
@@ -1,7 +1,16 @@
 /**
- * @brief [Period - Duration of a light, Time - Internal clock, Green - When the light turns green]
- * @details []
+ * <h1>Class Light!</h1>
  * 
+ * Period - How long the light is on, Time - Internal clock, Green - Compares to time and switches to green after a certain time.
+ */
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param period [description]
+ * @param green [description]
+ * 
+ * @return [description]
  */
 
 public class Light {
@@ -9,51 +18,28 @@ public class Light {
     private int time;  // Intern klocka: 0, 1, ... period-1, 0, 1 ...
     private int green; // Signalen grön när time<green 
 
-
-/**
- * @brief []
- * @details []
- * 
- * @param period []
- * @param green []
- * 
- * @return []
- */
     public Light(int period, int green) {
-
+        this.period = period;
+        this.green = green;
     }
 
-
-/**
- * @brief []
- * @details []
- */
     public void step() { 
+        time += 1;
        // Stegar fram klocka ett steg
     }
 
-
-/**
- * @brief []
- * @details []
- * @return []
- */
     public boolean isGreen()   {
+        if(time < this.green) return true;
+        else return false;
 	// Returnerar true om time<green, annars false
     }
 
-
-/**
-* @brief []
-* @details []
-* @return []
-*/
     public String  toString() {
         if (this.isGreen()) {
-            return "[GREEN]";
+            return "[G]";
         }   
         else {
-            return "[RED]";
+            return "[R]";
         }
     }	
 }   
